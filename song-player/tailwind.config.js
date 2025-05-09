adb55004a324bfd31824327a8c41562f364d7b5b
Luka <lukapetrovic5934@gmail.com>
2025-05-09 02:28:38 +0200

    Initial commit
    First commit 9.5.2025

---
+++ song-player/tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    "./public/**/*.{html,js}",  // This scans all HTML and JS files inside the public folder
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
